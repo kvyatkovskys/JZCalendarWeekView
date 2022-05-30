5893b13a7da03d7e2b7bd49aa99f76de05abe514
kvyatkovskys <sergejkvyatkovskij@gmail.com>
2022-05-30 15:37:48 +0300

    corrections#11

--- JZCalendarWeekView/Utils/JZWeekViewColors.swift
+++ JZCalendarWeekView/Utils/JZWeekViewColors.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 /// cannot change for now, will implement theme in the future
-class JZWeekViewColors {
+open class JZWeekViewColors {
 
     class var columnHeaderWeekday: UIColor { return UIColor(hex: 0x757575) }
     class var columnHeaderDay: UIColor { return UIColor(hex: 0x757575) }
